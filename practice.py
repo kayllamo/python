ff864645d660d9ce8a73da76a1d656f3d348271e
kaylagraham <kaylagraham@kaylas-mbp.attlocal.net>
2023-12-27 11:35:34 -0800

    python exercises

---
+++ practice.py
@@ -0,0 +1,13 @@
+print("Hello")
+
+age = 19
+
+if age >= 21:
+    print("you can drink")
+elif age < 18:
+    print('you cannot drink')
+
+if age > 30:
+    print('you are old')
+else:
+    print('yay come on in')
